0c7b730f77fafe8a2dac155d2c54e23e9c75d782
hagaiw <hagai@lightricks.com>
2017-02-23 22:18:10 +0200

    awww

--- data_parser.py
+++ data_parser.py
@@ -1,6 +1,20 @@
 from heapq import *
 import numpy as np
 
+def best_request(cache_latencies, endpoint_id, video_id):
+    best_score = -1
+    best_cache_id = -1
+
+    for cache_id, cache_latency in cache_latencies.iteritems():
+        score = video_request_count * (endpoints[endpoint_id]["center"] - cache_latency)
+        if score > best_score:
+            best_score = score
+            best_cache_id = cache_id
+
+    if best_score > 0:
+        return (-best_score, (video_id, endpoint_id, best_cache_id, cache_latencies))
+    return None
+
 f = open("me_at_the_zoo.in")
 data = f.readlines()
 
@@ -49,22 +63,64 @@ while line_number < len(data):
         video_size = video_sizes[video_id]
         endpoint = endpoints[endpoint_id]
 
-        best_score = -1
-        best_cache_id = -1
-        for cache_id, cache_latency in endpoint["caches"].iteritems():
-            score = video_request_count * (endpoint["center"] - cache_latency)
-            if score > best_score:
-                best_score = score
-                best_cache_id = cache_id
+        # best_score = -1
+        # best_cache_id = -1
+        # for cache_id, cache_latency in endpoint["caches"].iteritems():
+        #     score = video_request_count * (endpoint["center"] - cache_latency)
+        #     if score > best_score:
+        #         best_score = score
+        #         best_cache_id = cache_id
+        #
+        # if best_score > 0:
+        #     heappush(requests, (-best_score, (video_id, endpoint_id, best_cache_id)))
 
-        if best_score > 0:
-            heappush(requests, (-best_score, (video_id, endpoint_id, best_cache_id)))
+        request = best_request(endpoint["caches"], endpoint_id, video_id)
+
+        if request != None:
+            heappush(requests, request)
 
         line_number += 1
         continue
 
 
-print heappop(requests)
-print heappop(requests)
-print heappop(requests)
+remaining_cache_size = []
+cache_videos = []
+
+for i in xrange(0, cache_count):
+    remaining_cache_size.append(cache_capacity)
+    cache_videos.append([])
+
+while len(requests) > 0:
+    request = heappop(requests)
+    video_id, endpoint_id, cache_id, cache_latencies = request[1]
+    video_size = video_sizes[video_id]
+
+    print "request: " + str(request[1]) + "   video size: " + str(video_size)
+    print "remaining cache sizes:" +str(remaining_cache_size)
+    print cache_videos
+
+    if not video_id in cache_videos[cache_id]:
+        if remaining_cache_size[cache_id] > video_size:
+            cache_videos[cache_id].append(video_id)
+            remaining_cache_size[cache_id] = remaining_cache_size[cache_id] - video_size
+        else:
+            caches = cache_latencies.copy()
+
+            print "caches: " + str(caches)
+            caches.pop(cache_id)
+
+
+            new_request = best_request(caches, endpoint_id, video_id)
+            if new_request != None:
+                heappush(requests, new_request)
+
+
+
+print remaining_cache_size
+print cache_videos
+
+print "=" * 20
+print len(cache_videos)
+for i in xrange(1, len(cache_videos)):
+    print i, " ".join(map(str,cache_videos[i]))
 
