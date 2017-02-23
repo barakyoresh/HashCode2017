4cd1bddda7a7ece2b3e47d342f75fbf8c7c198d0
hagaiw <hagai@lightricks.com>
2017-02-23 22:59:29 +0200

    before optimisations

--- data_parser.py
+++ data_parser.py
@@ -1,6 +1,21 @@
 from heapq import *
 import numpy as np
 
+import pickle
+
+file_name = "kittens.in"
+# file_name = "me_at_the_zoo.in"
+# file_name = "trending_today.in"
+# file_name = "videos_worth_spreading.in"
+
+output_file_name = file_name + ".out"
+
+
+pickled_file_name = file_name + '_requests'
+
+
+parse = True
+
 def best_request(cache_latencies, endpoint_id, video_id):
     best_score = -1
     best_cache_id = -1
@@ -15,7 +30,8 @@ def best_request(cache_latencies, endpoint_id, video_id):
         return (-best_score, (video_id, endpoint_id, best_cache_id, cache_latencies))
     return None
 
-f = open("me_at_the_zoo.in")
+
+f = open(file_name)
 data = f.readlines()
 
 numbers = map(int, data[0].split(" "))
@@ -28,59 +44,72 @@ cache_capacity = int(numbers[4])
 
 video_sizes = map(int, data[1].split(" "))
 
+if parse:
 
-parsed_endpoint_count = 0
-parsed_request_count = 0
+    parsed_endpoint_count = 0
+    parsed_request_count = 0
 
-endpoints = []
+    endpoints = []
 
-# mat = np.zeros((video_count, endpoint_count, cache_count))
+    # mat = np.zeros((video_count, endpoint_count, cache_count))
 
-requests = []
+    requests = []
 
-line_number = 2
-while line_number < len(data):
-    if parsed_endpoint_count < endpoint_count:
-        endpoint = {}
-        endpoint["center"], num_caches = map(int, data[line_number].split(" "))
+    line_number = 2
+    while line_number < len(data):
+        # print "line number: ", str(line_number)
+        if parsed_endpoint_count < endpoint_count:
+            endpoint = {}
+            endpoint["center"], num_caches = map(int, data[line_number].split(" "))
 
-        caches = {}
+            caches = {}
 
-        for cahce_line_number in xrange(line_number + 1, line_number + 1 + num_caches):
-            cache_index, cache_latency = map(int, data[cahce_line_number].split(" "))
-            caches[cache_index] = cache_latency
+            for cahce_line_number in xrange(line_number + 1, line_number + 1 + num_caches):
+                cache_index, cache_latency = map(int, data[cahce_line_number].split(" "))
+                caches[cache_index] = cache_latency
 
-        endpoint["caches"] = caches
-        endpoints.append(endpoint)
+            endpoint["caches"] = caches
+            endpoints.append(endpoint)
 
-        parsed_endpoint_count += 1
-        line_number += num_caches + 1
-        continue
+            parsed_endpoint_count += 1
+            line_number += num_caches + 1
+            continue
 
-    if parsed_request_count < request_count:
-        video_id, endpoint_id, video_request_count = map(int, data[line_number].split(" "))
+        if parsed_request_count < request_count:
+            video_id, endpoint_id, video_request_count = map(int, data[line_number].split(" "))
 
-        video_size = video_sizes[video_id]
-        endpoint = endpoints[endpoint_id]
+            video_size = video_sizes[video_id]
+            endpoint = endpoints[endpoint_id]
 
-        # best_score = -1
-        # best_cache_id = -1
-        # for cache_id, cache_latency in endpoint["caches"].iteritems():
-        #     score = video_request_count * (endpoint["center"] - cache_latency)
-        #     if score > best_score:
-        #         best_score = score
-        #         best_cache_id = cache_id
-        #
-        # if best_score > 0:
-        #     heappush(requests, (-best_score, (video_id, endpoint_id, best_cache_id)))
+            # best_score = -1
+            # best_cache_id = -1
+            # for cache_id, cache_latency in endpoint["caches"].iteritems():
+            #     score = video_request_count * (endpoint["center"] - cache_latency)
+            #     if score > best_score:
+            #         best_score = score
+            #         best_cache_id = cache_id
+            #
+            # if best_score > 0:
+            #     heappush(requests, (-best_score, (video_id, endpoint_id, best_cache_id)))
 
-        request = best_request(endpoint["caches"], endpoint_id, video_id)
+            request = best_request(endpoint["caches"], endpoint_id, video_id)
 
-        if request != None:
-            heappush(requests, request)
+            if request != None:
+                heappush(requests, request)
 
-        line_number += 1
-        continue
+            line_number += 1
+            continue
+
+
+print "parsed"
+
+#     with open(pickled_file_name, 'wb') as fp:
+#         pickle.dump(requests, fp)
+#
+# with open (pickled_file_name, 'rb') as fp:
+#     pickledRequests = pickle.load(fp)
+
+# requests = pickledRequests
 
 
 remaining_cache_size = []
@@ -90,14 +119,15 @@ for i in xrange(0, cache_count):
     remaining_cache_size.append(cache_capacity)
     cache_videos.append([])
 
+
+
 while len(requests) > 0:
+
+    # print len(requests)
+
     request = heappop(requests)
     video_id, endpoint_id, cache_id, cache_latencies = request[1]
     video_size = video_sizes[video_id]
-
-    print "request: " + str(request[1]) + "   video size: " + str(video_size)
-    print "remaining cache sizes:" +str(remaining_cache_size)
-    print cache_videos
 
     if not video_id in cache_videos[cache_id]:
         if remaining_cache_size[cache_id] > video_size:
@@ -106,7 +136,6 @@ while len(requests) > 0:
         else:
             caches = cache_latencies.copy()
 
-            print "caches: " + str(caches)
             caches.pop(cache_id)
 
 
@@ -116,11 +145,23 @@ while len(requests) > 0:
 
 
 
-print remaining_cache_size
-print cache_videos
+# print remaining_cache_size
+# print cache_videos
+#
+# print "=" * 20
+# print len(cache_videos)
+# for i in xrange(0, len(cache_videos)):
+#     print i, " ".join(map(str,cache_videos[i]))
 
-print "=" * 20
-print len(cache_videos)
+
+output_array = [str(len(cache_videos))]
 for i in xrange(0, len(cache_videos)):
-    print i, " ".join(map(str,cache_videos[i]))
+    output_array.append(str(i) + " " +  " ".join(map(str,cache_videos[i])))
 
+
+output_file = open(output_file_name, "w")
+output_file.write("\n".join(map(str, output_array)))
+output_file.close()
+
+
+print "DONE DONE DONE"
